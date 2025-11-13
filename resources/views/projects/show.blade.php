2c139a692d156ce2792834e4b38a3cb2707a68f7
Baomont <166191870+Baomont@users.noreply.github.com>
2025-11-13 16:40:49 -0500

    Update show.blade.php

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -3912,7 +3912,7 @@
             wsHost: reverbConfig.wsHost,
             wsPort: reverbConfig.wsPort,
             wssPort: reverbConfig.wssPort,
-            wsPath: '/reverb',   //  ⭐ REQUIRED ⭐
+            wsPath: '/app',
             forceTLS: reverbConfig.forceTLS === 'true' || reverbConfig.forceTLS === true,
             enabledTransports: reverbConfig.enabledTransports,
             disableStats: true,
